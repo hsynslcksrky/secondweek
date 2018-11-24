141ff640bca6d374e0d6eb06df90560e42118c87
ahmetanbar <ahmetanbar@icloud.com>
2018-11-24 11:11:04 +0300

    initial commit

---
+++ onediotesti.c.c
@@ -0,0 +1,217 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+	char b[100];
+	char v;
+	int sayi;
+	int k=0,e=0,i=0,m=0,an=0,g=0,d=0,q=0,u=0;
+	int a=5;
+	int karar;
+	
+buraya:	printf("			NERELISIN BAKALIM\n\n\n");
+	
+	while(!(a==1))
+	{
+	if(a==2)
+	{
+		printf("%c yi nerede okudun soyler misin? :( \nBir daha soruyorum\n",b[0]);
+	}
+	printf("Olsa da yesek desem kacinciyi secerdin?\n");
+	printf("1-Dondurma\n2-Baklava\n3-Kebap\n4-Borek\n5-Cikolata\n\n");
+	scanf("%s",&b[0]);
+	printf("\n");
+	
+	if( b[0]=='2' ||b[0]=='3' || b[0]=='4' || b[0]=='5' || b[0]=='1' )
+	{
+		a=1;
+	}
+	else
+	{
+		a=2;
+	}
+
+	}
+	
+	v=b[0];
+	
+	
+
+		switch(v)
+	{
+		case '1': m++; e++;	break;	
+		case '2': i++; d++;   break;
+		case '3': g++; d++;   break;
+		case '4':m++; e++; a++; k++; i++; break;
+		case '5': e++; an++; m++; u++; break;
+	}	
+	
+	a=5;
+	
+	
+	while(!(a==1))
+	{
+
+			if(a==2)
+		{
+			printf("%c yi nerede okudun soyler misin? :( \nBir daha soruyorum\n",b[0]);
+		}
+		printf("Peki tmm.Su yabanci sehirlerden hangisinde yasamak isterdin?\n");
+		printf("1-Newyork\n2-Paris\n3-Londra\n4-Amsterdam\n5-Teksas\n\n");
+		scanf("%s",&b[0]);
+		printf("\n");
+		
+		if( b[0]=='2' ||b[0]=='3' || b[0]=='4' || b[0]=='5' || b[0]=='1' )
+		{
+			a=1;
+		}
+		else
+		{
+			a=2;
+		}
+
+	}
+
+	v=b[0];
+	
+
+		switch(v)
+	{
+		case '1':  	m=m+1; 		e++;		break;	
+		case '2': an++; m++;  break;
+		case '3': g++; d++; i++;  break;
+		case '4': e++; an++; k++; break;
+		case '5': d++; g++; u++;  break;
+	}
+	
+	a=5;
+	
+	while(!(a==1))
+	{
+	if(a==2)
+	{
+		printf("%c yi nerede okudun soyler misin? :( \nBir daha soruyorum\n",b[0]);
+	}
+	printf("Buda tmm.Peki hangisinde yuzmek isterdin?\n");
+	printf("1-Deniz\n2-Havuz\n3-Kamyonun kasasinda\n4-Derede\n5-Fark etmez haci\n\n");
+	scanf("%s",&b[0]);
+	printf("\n");
+	
+	if( b[0]=='2' ||b[0]=='3' || b[0]=='4' || b[0]=='5' || b[0]=='1' )
+	{
+		a=1;
+	}
+	else
+	{
+		a=2;
+	}
+
+	}
+	
+	v=b[0];
+
+		switch(v)
+	{
+		case '1': m++; an++;	break;	
+		case '2': e++;   break;
+		case '3': k++; d++;  break;
+		case '4': k++; d++; break;
+		case '5': k++; u++; break;
+	}	
+	
+	a=5;
+	
+	while(!(a==1))
+	{
+	if(a==2)
+	{
+		printf("%c yi nerede okudun soyler misin? :( \nBir daha soruyorum\n",b[0]);
+	}
+	printf("Denize dusse hangisini once kurtarirsin(sevgililer gozlerini yumsun bi pilis\n");
+	printf("1-Akilli telefonumu\n2-Sevgilimi\n3-Kararsizim\n4-Hic birini\n5-Denizi buldukta kurtarmak kaldi\n\n");
+	scanf("%s",&b[0]);
+	printf("\n");
+	
+	if( b[0]=='2' ||b[0]=='3' || b[0]=='4' || b[0]=='5' || b[0]=='1' )
+	{
+		a=1;
+	}
+	else
+	{
+		a=2;
+	}
+
+	}
+	
+	v=b[0];
+
+		switch(v)
+	{
+		case '1': m++; e++;	break;	
+		case '2': k++; d++;   break;
+		case '3': an++; g++;   break;
+		case '4': g++; d++; k++; break;
+		case '5': i++; g++; d++; u++; break;
+	}	
+	
+	a=5;
+	
+		while(!(a==1))
+	{
+	if(a==2)
+	{
+		printf("%c yi nerede okudun soyler misin? :( \nBir daha soruyorum\n",b[0]);
+	}
+	printf("Senin hayatin hangi sanatla anlatilsin isterdin?\n");
+	printf("1-Roman\n2-Siir\n3-Muzik\n4-Opera\n5-Anlatmasinlar\n\n");
+	scanf("%s",&b[0]);
+	printf("\n");
+	
+	if( b[0]=='2' ||b[0]=='3' || b[0]=='4' || b[0]=='5' || b[0]=='1' )
+	{
+		a=1;
+	}
+	else
+	{
+		a=2;
+	}
+
+	}
+	
+	v=b[0];
+
+		switch(v)
+	{
+		case '1': m++; e++;	break;	
+		case '2': e++; m++; i++;   break;
+		case '3': k; d;   break;
+		case '4': e++; an++; break;
+		case '5': g; d; k; u++; break;
+	}	
+	
+	a=5;
+	
+	system("cls");
+	printf("Bi dk dusunuyorum ...\n\n");
+	printf("%%%d Karadenizlisin\n",(100*k)/(an+m+e+i+u+d+g+k));
+	printf("%%%d Dogu anadolulusun\n",(100*d)/(an+m+e+i+u+d+g+k));
+	printf("%%%d Egelisin\n",(100*e)/(an+m+e+i+u+d+g+k));
+	printf("%%%d Marmaralisin\n",(100*m)/(an+m+e+i+u+d+g+k));
+	printf("%%%d Guneydogu Anadolulusun\n",(100*g)/(an+m+e+i+u+d+g+k));
+	printf("%%%d Ýc Anadolulusun\n",(100*i)/(an+m+e+i+u+d+g+k));
+	printf("%%%d Uzaylisin\n",(100*u)/(an+m+e+i+u+d+g+k));
+	printf("%%%d Akdenizlisin\n\n",(100*an)/(an+m+e+i+u+d+g+k));
+	
+	printf("Tekrar Cozmek icin 61 e\nCikmak icin ne yapican bilmiyom bas biseylere iste :/ \n");
+	scanf("%d",&karar);
+
+switch(karar)
+{
+	case 61: printf("peki seni kirmayacagim..\n\n"); goto buraya;
+}
+	
+	
+	
+}
